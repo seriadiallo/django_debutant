ca1491a4873ce4f70ba79ad4ccbc24f9e15d7d2b
Sadialiou Seria <seriadiallo1@gmail.com>
2020-02-04 18:33:10 +0000

    ajout des models de articles et creation des  migrations :

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+
+
+    titre = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    date_pub = models.DateTimeField(null=True)
+    auteur = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.titre
+    
+
+
+class Commentaire(models.Model):
+
+    contenu = models.TextField(null=False)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
